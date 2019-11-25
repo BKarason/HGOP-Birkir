65c03c41078f04fce7a03276d70a5a7a07e52ed6
Birkir <birkir17@ru.is>
2019-11-25 14:27:38 +0000

    Got date to work

--- scripts/setup_local_dev_environment.sh
+++ scripts/setup_local_dev_environment.sh
@@ -5,3 +5,15 @@ printf "Hello, $USER! \n\n"
 
 #Description of the script.
 printf "This is an installation script for git and node on your $OSTYPE system!\n\n"
+
+#Storing date and time at the beginning of installation
+begin=$(date)
+
+#Display date and time of installation
+printf "Beginning installation at: $begin\n\n"
+
+#Storing date and time at the end of installation
+end=$(date)
+
+#Display date and time of installation end
+printf "Installation finished at: $end\n\n"
