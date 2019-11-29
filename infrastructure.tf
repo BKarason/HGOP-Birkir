b2204d708dcd1a19cc1459d5a8739659d0f66ad2
Birkir <birkir17@ru.is>
2019-11-29 16:36:21 +0000

    Commented all TODO parts of the infrastructure file

--- infrastructure.tf
+++ infrastructure.tf
@@ -1,10 +1,11 @@
-# TODO Comment 2-3 sentences.
+# Use AWS as the provider and location of credentials
 provider "aws" {
   shared_credentials_file = "~/.aws/credentials"
   region                  = "us-east-1"
 }
 
-# TODO Comment 2-3 sentences.
+# Create security group named game_security_group and open
+# ports for incoming and outgoing traffic
 resource "aws_security_group" "game_security_group" {
   name = "GameSecurityGroup"
 
@@ -30,7 +31,8 @@ resource "aws_security_group" "game_security_group" {
   }
 }
 
-# TODO Comment 2-3 sentences.
+# Create instance of AWS named game_server using the provided
+# Ubuntu 18.04 LTS of type micro Amazon Machine Image.
 resource "aws_instance" "game_server" {
   ami                    = "ami-0ac019f4fcb7cb7e6"
   instance_type          = "t2.micro"
@@ -40,7 +42,8 @@ resource "aws_instance" "game_server" {
     Name = "GameServer"
   }
 
-  # TODO Comment 1-2 sentences.
+  # Copy initialize_game_api_instance script from local machine
+  # To the AWS instance through a ssh connection
   provisioner "file" {
     source      = "scripts/initialize_game_api_instance.sh"
     destination = "/home/ubuntu/initialize_game_api_instance.sh"
@@ -53,7 +56,8 @@ resource "aws_instance" "game_server" {
     }
   }
 
-  # TODO Comment 1-2 sentences.
+  # Copy docker-compose file from local machine
+  # To the AWS instance through a ssh connection
   provisioner "file" {
     source      = "docker-compose.yml"
     destination = "/home/ubuntu/docker-compose.yml"
@@ -71,7 +75,8 @@ resource "aws_instance" "game_server" {
   # Since it can take time for the SSH agent on machine to start up we let Terraform
   # handle the retry logic, it will try to connect to the agent until it is available
   # that way we know the instance is available through SSH after Terraform finishes.
-  # TODO Comment 1-2 sentences.
+  # Give the initialize_game_api_instance script executeble permission so it can be run
+  # on the AWS instance
   provisioner "remote-exec" {
     inline = [
       "chmod +x /home/ubuntu/initialize_game_api_instance.sh",
@@ -86,7 +91,7 @@ resource "aws_instance" "game_server" {
   }
 }
 
-# TODO Comment 1-2 sentences.
+# Output the public ip of the AWS instance running the API
 output "public_ip" {
   value = aws_instance.game_server.public_ip
 }
