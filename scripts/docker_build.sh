fa7282e83b27c9f5eb3d3d2d318bb64872ef3776
Birkir <birkir17@ru.is>
2019-12-03 17:31:35 +0000

    gave docker scripts executable permissions

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -10,5 +10,5 @@ docker build -t bkarason/hgop:$GIT_COMMIT item_repository/
 # keep track of the last executed command
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
-trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+trap 'echo "\"${last_command}\" command failed with exit code $?."' EXIT
 
--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -10,4 +10,4 @@ docker push bkarason/hgop:$GIT_COMMIT
 # keep track of the last executed command
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
-trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+trap 'echo "\"${last_command}\" command failed with exit code $?."' EXIT
