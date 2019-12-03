66082712873d05dbfc51fe4ef4857e24cafa4198
Birkir <birkir17@ru.is>
2019-12-03 18:36:34 +0000

    trying to figure out why build fails

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # exit on error if any command fails
-set -e
+#set -e
 
 GIT_COMMIT=$1
 
 docker push bkarason/hgop:$GIT_COMMIT
 
 # keep track of the last executed command
-trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+#trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
-trap 'echo "\"${last_command}\" command failed with exit code $?."' EXIT
+#trap 'echo "\"${last_command}\" command failed with exit code $?."' EXIT
