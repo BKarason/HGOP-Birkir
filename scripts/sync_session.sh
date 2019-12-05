2ab41fe56384998f083434773a0060f866431be4
Birkir <birkir17@ru.is>
2019-12-05 22:08:02 +0000

    trigger build

--- scripts/sync_session.sh
+++ scripts/sync_session.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-JENKINS_URL="ec2-52-90-72-24.compute-1.amazonaws.com"
+JENKINS_URL="ec2-34-201-166-31.compute-1.amazonaws.com"
 
 scp -o StrictHostKeyChecking=no -i "~/.aws/JenkinsKeyPair.pem" ~/.aws/credentials ubuntu@${JENKINS_URL}:~/credentials
 ssh -o StrictHostKeyChecking=no -i "~/.aws/JenkinsKeyPair.pem" ubuntu@${JENKINS_URL} "sudo mv ~/credentials /var/lib/jenkins/.aws/credentials"
