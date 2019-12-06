04418ed364e0370fc89dc1ae37a854ee4351a581
Birkir <birkir17@ru.is>
2019-12-06 16:24:25 +0000

    updated docker compose and added some much needed dockerfile changes that had been forgotten

--- game_api/dealer.js
+++ game_api/dealer.js
@@ -1,17 +1,20 @@
-module.exports = () => {
-    return {
-        shuffle: (deck, random) => {
-            for (let i = 0; i < deck.length - 1; i++) {
-                const j = Math.floor(random() * (deck.length - i)) + i;
-                const card = deck[j];
-                const old = deck[i];
-                deck[i] = card;
-                deck[j] = old;
-            }
-        },
-        draw: (deck) => {
-            const card = deck.pop();
-            return card;
-        },
-    };
+// dealer.js
+module.exports = (context) => {
+  let randomConstructor = context('random');
+  let random = randomConstructor(context);
+  return {
+    shuffle: (deck, random) => {
+        for (let i = 0; i < deck.length - 1; i++) {
+          const j = Math.floor(random() * (deck.length - i)) + i;
+          const card = deck[j];
+          const old = deck[i];
+          deck[i] = card;
+          deck[j] = old;
+        }
+    },
+    draw: (deck) => {
+      const card = deck.pop();
+      return card;
+    },
+  };
 };
