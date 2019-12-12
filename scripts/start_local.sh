c1f8333d2900fee60d16eb414a7ff453eaa365a5
Birkir <birkir17@ru.is>
2019-12-12 13:25:52 +0000

    still working on pipeline fail

--- scripts/start_local.sh
+++ scripts/start_local.sh
@@ -4,6 +4,6 @@ set -euxo pipefail
 
 docker build game_api -t bkarason/hgop:dev
 (cd game_client && npm run build)
-docker build game_client -t bkarason/hgop-cli:dev
+docker build game_client -t bkarason/hgopcli:dev
 
 API_URL=http://localhost:3000 GIT_COMMIT=dev docker-compose up
